91caf44c8ff28cb6820589f46a946668762471cd
Reinert <reilemx@gmail.com>
2018-08-06 17:42:12 +0200

    Working file fetch

--- lib/ios/AIRGoogleMapLocalTileOverlay.m
+++ lib/ios/AIRGoogleMapLocalTileOverlay.m
@@ -15,14 +15,14 @@
 
 - (void)requestTileForX:(NSUInteger)x y:(NSUInteger)y zoom:(NSUInteger)zoom receiver:(id<GMSTileReceiver>)receiver {
     NSString *path = self.pathTemplate;
-    if (path == nil) {
-        [receiver receiveTileWithX:x y:y zoom:zoom image:nil];
-    }
     path = [path stringByReplacingOccurrencesOfString:@"{x}" withString:[NSString stringWithFormat: @"%ld", (long)x]];
     path = [path stringByReplacingOccurrencesOfString:@"{y}" withString:[NSString stringWithFormat: @"%ld", (long)(1 << zoom) - 1 - y]];
     path = [path stringByReplacingOccurrencesOfString:@"{z}" withString:[NSString stringWithFormat: @"%ld", (long)zoom]];
     NSURL *filePath = [NSURL URLWithString:path];
-    printf("LOCALTILE %s\n", [filePath.path UTF8String]);
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath.path]) {
+        UIImage* image = [UIImage imageWithData:[NSData dataWithContentsOfFile:filePath.path]];
+        [receiver receiveTileWithX:x y:y zoom:zoom image:image];
+    }
 }
 
 @end
